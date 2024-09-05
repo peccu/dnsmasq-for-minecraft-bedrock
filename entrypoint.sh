f7df6cbd60d8db6704d741b10f7bc37ad2328d9e
peccu <peccul+github@gmail.com>
2024-09-05 21:05:36 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-cat <<EOF >> /etc/dnsmasq.conf
-domain-needed
-bogus-priv
-strict-order
-bind-interfaces
-EOF
+cat dnsmasq.conf >> /etc/dnsmasq.conf
 
 for i in geo.hivebedrock.network play.galaxite.net mco.mineplex.com mco.cubecraft.net play.pixelparadise.gg mco.lbsg.net play.inpvp.net
 do
 echo "$MINECRAFT_SERVER $i" >> /etc/hosts
 done
-exec dnsmasq -k
+exec dnsmasq -kdq
