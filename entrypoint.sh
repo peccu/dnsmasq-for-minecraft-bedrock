576f6049aed5e0b35bd2e6545a7730aa1f904321
peccu <peccul+github@gmail.com>
2024-09-05 21:41:38 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 cat dnsmasq.conf >> /etc/dnsmasq.conf
+awk "{print \"$MINECRAFT_SERVER\" \$0}" <hosts >>/etc/hosts
 
-for i in geo.hivebedrock.network play.galaxite.net mco.mineplex.com mco.cubecraft.net play.pixelparadise.gg mco.lbsg.net play.inpvp.net
-do
-echo "$MINECRAFT_SERVER $i" >> /etc/hosts
-done
 nohup dnsmasq -kdq | cat -
 
 /usr/local/bin/entrypoint-demoter --match /data --debug --stdin-on-term stop /opt/bedrock-entry.sh
