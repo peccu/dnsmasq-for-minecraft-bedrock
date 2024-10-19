2fcf3f2eeb4b762f2867eff7e2909e13086f3ccb
peccu <peccul+github@gmail.com>
2024-10-19 16:11:59 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 cat /dnsmasq.conf >> /etc/dnsmasq.conf
 
 ipv4_pattern='^([0-9]{1,3}\.){3}[0-9]{1,3}$'
-if [[ $input =~ $ipv4_pattern ]]
+if [[ $MINECRAFT_SERVER =~ $ipv4_pattern ]]
 then
   awk "{print \"$MINECRAFT_SERVER \" \$0}" </hosts >>/etc/hosts
 else
